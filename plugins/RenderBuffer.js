d36d9dff76c81524e666d613c258a7acb7a54373
Mario Pietsch <pmariojo@gmail.com>
2012-02-06 21:21:01 +0100

    added cmBuffer handling. including find params

---
+++ plugins/RenderBuffer.js
@@ -0,0 +1,185 @@
+/***
+|''Name''|RenderBufferPlugin|
+|''Description''||
+|''Author''|PMario|
+|''Version''|0.0.1|
+|''Status''|@@experimental@@|
+|''Source''||
+|''License''|[[CC by-nc-sa 3.0|http://creativecommons.org/licenses/by-nc-sa/3.0/]]|
+|''CoreVersion''|2.5.0|
+|''Keywords''|render wikify buffer tiddler|
+!Documentation
+<<<
+..
+<<<
+!!!Usage
+..
+<<<
+***/
+//{{{
+
+version.extensions.RenderBufferPlugin = {
+	major: 0,
+	minor: 0,
+	revision: 1,
+	date: new Date(2012, 2, 6)
+};
+
+(function ($) {
+	var me;
+
+	config.macros.renderBuffer = me = {
+		// should be done for easy localisation
+		locale: {
+			txtBtnTooltip: "Not valid at the moment."
+		},
+		
+		// converts the named params into keys of an object. 
+		// Be aware, that parmName:true will be a text "true" variable, not a boolean !!
+		// See: https://developer.mozilla.org/en/JavaScript/Guide/Working_with_Objects
+		paramsToObject: function (conf, names, params, initValue) {			
+			var tmp; 
+			for (var i=0, im = names.length; i<im; i += 1) {
+				tmp = getParam( params, names[i], initValue);
+				
+				// the conf.xx variable will only be set if a parameter or "initValue" is defined;
+				// For my examples. I don t use initValue!!
+				if (tmp) {
+					conf[names[i]] = tmp;
+				} 
+			} // for
+		},
+
+		// Set some default values
+		defaults: {
+			start: 0,
+			lines: 10,
+			tag: 'span',
+			class: 'cmBuffer',
+			id: ''
+		},
+
+		handler: function (place, macroName, params, wikifier, paramString, tiddler) {
+			params = paramString.parseParams(null, null, true);
+			// console.log('params', params);
+			var btn;			// will be the button element.
+			var txtTooltip;			// button tooltip is a helper variable to make the code more readable
+			var conf = {};			// contains the named params, to work with.
+
+			// these are optional params which are used by your plugin.
+			// no 'place' param is allowed, it will be overwritten.
+			var names = ['text', 'button', 'id', 'tiddler', 'start', 'lines', 'find', 'next'];
+
+			// define the default values, if needed. See: http://api.jquery.com/jQuery.extend/
+			$.extend(conf, me.defaults);
+	
+			// next function converts names into object keys.
+			// can be: me.paramsToObject(conf, names, params, initValue); 
+			// BUT initValue will overwrite me.defaults! code line above
+			me.paramsToObject(conf, names, params);
+
+            conf.place = place;        // place will be needed to find the containing element.
+			
+			// ============================================================================
+			// from here on, you can work with your "conf" object, that contains the params.
+	 		// console.log('conf: ', conf);
+
+			// start default line = 1 ==> index 0
+			conf.start = (conf.start > 0) ? parseInt(conf.start,10)-1 : 0; 
+			conf.lines = parseInt(conf.lines,10);
+			conf.tiddler = (conf.tiddler) ? conf.tiddler : null;
+			
+			conf.output = createTiddlyElement(place, conf.tag, conf.id, conf.class);
+
+			$(conf.output).data('conf', conf).attr({"refresh":'macro', "macroname": macroName});
+
+			if (conf.button) {
+				txtTooltip = (conf.tooltip) ? conf.tooltip : me.locale.txtBtnTooltip;
+
+				// next comment line is used as a reminder for the possible/needed parameters
+				// createTiddlyButton(parent, text, tooltip, action, className, id, accessKey, attribs)			
+				btn = createTiddlyButton(place, conf.button, txtTooltip, me.onClick, 'button');
+
+				// adding a jQuery data object to the button. see: http://api.jquery.com/jQuery.data/
+				var result = $(btn).data('data', conf.output);
+
+				// var result is just there, to be used as a console.log var in the next line.
+				// console.log('result:', result);
+			} // if
+
+			if (!conf.button) me.refresh(conf.output);
+		}, // handler
+
+
+/*
+match = subject.match(/^!(?=[^!])/im);
+if (match != null) {
+	// matched text: match[0]
+	// match start: match.index
+	// capturing group n: match[n]
+} else {
+	// Match attempt failed
+}
+
+
+result = subject.match(/^!(?=[^!])/img);
+
+*/			
+		refresh: function (el) {
+			var tmp
+				, iMax
+				, conf = $(el).data('conf');
+						
+			// if tiddler is specified, text will not be used
+			conf.text = (conf.tiddler) ? store.getTiddlerText(conf.tiddler) : conf.text;
+
+			tmp = conf.text.split('\n');
+			iMax = (tmp.length >= conf.lines) ? conf.lines : tmp.length;
+
+			if (conf.find) {
+
+				iMax = tmp.length;
+				var regStart = new RegExp('^' + conf.find, 'im');
+				var regNext  = new RegExp('^' + conf.next, 'im');
+
+				for (i=0, iMax=tmp.length; i < iMax; i++ ) {
+//					match = tmp[i].match(/^!(?=[^!])/im);
+					match = tmp[i].match(regStart);
+					if (match != null) {
+						conf.start = i;
+						iMax = iMax - i;
+						i += 1;
+						break;
+					}
+				} // for
+
+				if (conf.next) {
+					for (iMax=tmp.length; i < iMax; i++ ) {
+						match = tmp[i].match(regNext);
+						if (match != null) {
+							iMax = i-conf.start;
+							break;
+						}
+					}
+				} // if 
+console.log('m: ', match, 'tmp: ', tmp, 'conf: ', conf, 'iMax', iMax);
+			} // if (conf.find)
+
+			if (conf.text) {
+				conf.text = tmp.slice(conf.start, conf.start+iMax).join('\n');
+			}
+
+			$(conf.output).empty();
+			wikify(conf.text, conf.output);
+		},
+
+		onClick: function() {
+			// since the handlers context is not defined in the right way, when the button is clicked
+			// it needs to be loaded from the jQuery data object. http://api.jquery.com/jQuery.data/
+			var data = $(this).data("data");
+			me.refresh(data);
+ 		}			
+	}; // end plugin
+})(jQuery);
+
+//}}}
